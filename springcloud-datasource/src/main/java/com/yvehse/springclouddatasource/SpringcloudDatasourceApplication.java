9e3cabf6a242e14f755745c3ebc476e53b246964
YvesHe <yveshe@aliyun.com>
2020-04-29 14:24:47 +0800

    + 简单数据模块-init代码

---
+++ springcloud-datasource/src/main/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.yvehse.springclouddatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudDatasourceApplication.class, args);
+    }
+
+}
---
+++ springcloud-datasource/src/test/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvehse.springclouddatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
