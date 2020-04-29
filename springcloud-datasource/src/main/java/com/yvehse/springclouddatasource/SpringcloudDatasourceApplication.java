fab582b45cd4329ff0275faa5bec988d024afe33
YvesHe <yveshe@aliyun.com>
2020-04-29 20:49:14 +0800

    * 针对初始化项目,改造成springcloud中使用简单数据源项目

--- springcloud-datasource/src/main/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplication.java
+++ springcloud-datasource/src/main/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplication.java
@@ -1,13 +1,53 @@
 package com.yvehse.springclouddatasource;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class SpringcloudDatasourceApplication {
+@Slf4j
+public class SpringcloudDatasourceApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudDatasourceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    /**
+     * 打印连接信息
+     *
+     * @throws SQLException
+     */
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info(conn.toString());
+        conn.close();
+    }
+
+
+    /**
+     * 通过jdbacTemplate的方式查询数据
+     */
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO").forEach(row -> log.info(row.toString()));
+    }
 }
