edfd908729d07acb29db2cc697101d50eeee59f5
YvesHe <yveshe@aliyun.com>
2020-04-30 15:03:20 +0800

    * 增加开启springboot的默认事务管理

--- springcloud-datasource/src/main/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplication.java
+++ springcloud-datasource/src/main/java/com/yvehse/springclouddatasource/SpringcloudDatasourceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 @Slf4j
+@EnableTransactionManagement //启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
 public class SpringcloudDatasourceApplication implements CommandLineRunner {
 
     @Autowired
